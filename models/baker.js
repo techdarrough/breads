79f8884d6f97761dc25a7921c59ca608d3d73e72
Preston Darrough <techdarrough@gmail.com>
2022-03-24 18:35:59 -0700

    added deleted button

--- models/baker.js
+++ models/baker.js
@@ -28,6 +28,14 @@ bakerSchema.virtual("breads", {
   foreignField: "baker",
 });
 
+// hooks 
+bakerSchema.post('findOneAndDelete', function() {
+  Bread.deleteMany({ baker: this._conditions._id })
+      .then(deleteStatus => {
+          console.log(deleteStatus)
+      })
+})
+
 // Model & Export
 const Baker = mongoose.model("Baker", bakerSchema);
 module.exports = Baker;
